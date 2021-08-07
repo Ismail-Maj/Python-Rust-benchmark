bf787a87c3998781859fa318441bf80937a49355
unknown <ismaj@protonmail.com>
2021-08-08 00:28:50 +0200

    remove unused import

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
-import sys
 
 
 def generate_boxes(width_max, height_max, size=20):
     width = np.random.geometric(7 / width_max, size=size).clip(max=width_max)
     height = np.random.geometric(7 / height_max, size=size).clip(max=height_max)
+
     x = np.random.randint(0, width_max - width + 1, size=size)
     y = np.random.randint(0, height_max - height + 1, size=size)
+
     return x, x + width, y, y + height
 
 
@@ -20,12 +21,13 @@ def draw_boxes(image, boxes, color=(0, 0, 0), thickness=2):
 def max_intersection(A, B):
     x1, x2, y1, y2 = A
     x3, x4, y3, y4 = B
-    x1, x3 = np.expand_dims(x1, axis=0), np.expand_dims(x3, axis=1)
-    y1, y3 = np.expand_dims(y1, axis=0), np.expand_dims(y3, axis=1)
-    x2, x4 = np.expand_dims(x2, axis=0), np.expand_dims(x4, axis=1)
-    y2, y4 = np.expand_dims(y2, axis=0), np.expand_dims(y4, axis=1)
-    x5, y5 = np.maximum(x1, x3), np.maximum(y1, y3)
-    x6, y6 = np.minimum(x2, x4), np.minimum(y2, y4)
+
+    x5 = np.maximum(np.expand_dims(x1, axis=0), np.expand_dims(x3, axis=1))
+    x6 = np.minimum(np.expand_dims(x2, axis=0), np.expand_dims(x4, axis=1))
+
+    y5 = np.maximum(np.expand_dims(y1, axis=0), np.expand_dims(y3, axis=1))
+    y6 = np.minimum(np.expand_dims(y2, axis=0), np.expand_dims(y4, axis=1))
+
     width, height = x6 - x5, y6 - y5
     area = width.clip(min=0) * height  # must clip one to avoid negative * negative -> positive
 
